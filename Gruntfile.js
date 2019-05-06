246605478fa23f34a4cc1ae525fd13d008ef6464
nicostran <nstranquist@gmail.com>
2019-05-05 19:10:14 -0500

    my final attempt at grunt... will revisit later later

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,69 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        task: {
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
             sass: {
-                dev: {
-                    files: {
-                        //destination       //source files
-                        './output.css': 'scss/main.scss'
-                    }
+                files: '**/*.scss', //** any directory; *anyfile
+                tasks: ['css']
+            },
+            concat: {
+                files: ['./output.css'],
+                tasks: ['css']
+            },
+            uglify: {
+                files: './output.js',
+                tasks: ['uglify']
+            },
+            /* for live reload:
+            all: {
+                files: ['./index.html'],
+                options: {
+                    livereload: true
+                }
+            }*/
+        },
+        sass: {
+            dev: {
+                files: {
+                    //destination       //source files
+                    './output.css': 'scss/main.scss'
+                }
+            }
+        },
+        cssmin: {
+            build: {
+                src: './scss/output.scss',
+                dest: './output.css'
+            }
+        },
+        concat: {
+            options: {
+                separator: '\n/*new file*/\n\n'
+            },
+            dist: {
+                src: [
+                    'scripts/script.js'
+                    //add other js files here
+                ],
+                dest: './output.js'
+            }
+        },
+        uglify: {
+            build: {
+                files: {
+                    './output.min.js': ['./output.js']
                 }
             }
         }
     });
 
-    grunt.registerTask(taskName, [optional description, ] taskFunction);
+    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('css', ['sass', 'cssmin']);
+    grunt.registerTask('js', ['concat', 'uglify']);
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 };
