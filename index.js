ac78b7f8017a902c1fe71878567944a2b59dc434
nicostran <nstranquist@gmail.com>
2019-05-07 22:56:48 -0500

    hello world get request running

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+//send GET request
+app.get('/', function(req,res) {
+    res.send({'hello': 'world'})
+})
+
+// listen on port 3000
 app.listen(3000, function() {
     console.log('Listening on port 3000');
 })
